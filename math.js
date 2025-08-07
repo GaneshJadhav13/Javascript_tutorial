e25af23db93ae3dd1d51f51bdbc5fe54a8dfc8f1
Ganesh Jadhav <ganeshjadhav1013@gmail.com>
2025-08-07 18:37:22 +0530

    math & tdz

---
+++ math.js
@@ -0,0 +1,13 @@
+console.log("PI:", Math.PI);                     // 3.141592...
+console.log("Square root of 16:", Math.sqrt(16)); // 4
+console.log("2 to the power 4:", Math.pow(2, 4)); // 16
+
+console.log("Absolute:", Math.abs(-50));         // 50
+console.log("Ceil:", Math.ceil(4.2));            // 5
+console.log("Floor:", Math.floor(4.9));          // 4
+console.log("Round:", Math.round(4.5));          // 5
+
+console.log("Max:", Math.max(1, 5, 10));         // 10
+console.log("Min:", Math.min(1, 5, 10));         // 1
+
+console.log("Random (0 to 1):", Math.random());  // 0.0 – 0.999
---
+++ tdz.js
@@ -0,0 +1,8 @@
+
+// Temporial dead zone 
+
+console.log(a);
+
+let a=10;// error is can not be access before initialization 
+
+//console.log(a);
